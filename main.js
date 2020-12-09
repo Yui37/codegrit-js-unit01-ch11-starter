b15b714bcd7f4c9962f69be3da1038f13855716e
Yui <ytkakentan@yahoo.co.jp>
2020-12-09 21:22:21 +0900

    need to add sth in if

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 // モーダル要素の取得
-
+const modal = document.getElementById('easyModal');
 
 // 開くボタン取得
-
+const open = document.getElementById('modalOpen');
 
 // 閉じるボタン取得
-
-
-
-// クリックで開くイベントリスナー
-
-
-// クリックで閉じるイベントリスナー
-
-
-// モーダル以外の場所クリックのイベントリスナー
-
-
+const close = document.getElementById('modalClose');
 
 // モーダルオープンの関数
+function modalOpen() {
+    modal.style.display = "block";
+}
 
+// クリックで開くイベントリスナー
+open.addEventListener('click', modalOpen);
 
 // モーダルを閉じる関数
+function modalClose() {
+    modal.style.display = "none";
+}
+// クリックで閉じるイベントリスナー
+close.addEventListener('click', modalClose);
 
+// モーダル以外がクリックされた時に閉じる関数 ifを使う
+function anotherClose() {
+    if (){
+        modal.style.display = "none";
+    }else{
+        modal.style.display = "block";
+    }
+}
 
-// モーダル以外がクリックされた時に閉じる関数
+// モーダル以外の場所クリックのイベントリスナー
+modal.addEventListener('click', anotherClose);
+
+// // HTMLのmodalCloseをclass属性からid属性に変えちゃった
